87a3f7cf827a8db26eb7e07ceb929614e285d831
satthukid000 <satthubongmoi2305@gmail.com>
2024-10-05 02:39:44 +0700

    update: add SortWord class and mongo manager

---
+++ mini_game/SortWord/SwClass.py
@@ -0,0 +1,169 @@
+from typing import List, Optional
+#region SortWordInfo
+class SortWordInfo:
+    def __init__(self, channel_id: int, channel_name: str, current_player_id: int = None, current_player_name: str = None, unsorted_word: str = None, current_word: str = None, special_point: int = None, special_item: Optional['SwSpecialItem'] = None, used_words: List[str] = None, special_case: bool = False, player_profiles: Optional[List['SwPlayerProfile']] = None, player_effects : Optional[List['SwPlayerEffect']] = None, player_bans : Optional[List['SwPlayerBan']] = None):
+        self.channel_id = channel_id
+        self.channel_name = channel_name
+        self.current_player_id = current_player_id
+        self.current_player_name = current_player_name
+        self.unsorted_word = unsorted_word
+        self.current_word = current_word
+        self.special_point = special_point
+        self.special_item = special_item if special_item else None
+        self.special_case = special_case
+        self.used_words: List[str] = used_words if used_words else []
+        self.player_profiles: List[SwPlayerProfile] = player_profiles if player_profiles else []
+        self.player_effects: List[SwPlayerEffect] = player_effects if player_effects else []
+        self.player_bans: List[SwPlayerBan] = player_bans if player_bans else []
+    
+    def to_dict(self):
+        return {
+            "channel_id": self.channel_id,
+            "channel_name": self.channel_name,
+            "current_player_id": self.current_player_id,
+            "current_player_name": self.current_player_name,
+            "unsorted_word": self.unsorted_word,
+            "current_word": self.current_word,
+            "special_point": self.special_point,
+            "special_case": self.special_case,
+            "special_item": self.special_item.to_dict() if self.special_item else None,
+            "used_words": [data for data in self.used_words],
+            "player_profiles": [data.to_dict() for data in self.player_profiles],
+            "player_effects": [data.to_dict() for data in self.player_effects],
+            "player_bans": [data.to_dict() for data in self.player_bans],
+        }
+
+    @staticmethod
+    def from_dict(data:dict):
+        return SortWordInfo(
+            channel_id=data.get("channel_id", None),
+            channel_name=data.get("channel_name", None),
+            current_player_id=data.get("current_player_id", None),
+            current_player_name=data.get("current_player_name", None),
+            unsorted_word=data.get("unsorted_word", None),
+            current_word=data.get("current_word", None),
+            special_point=data.get("special_point", None),
+            special_case=data.get("special_case", False),
+            point=data.get("point", 0),
+            special_items = [SwSpecialItem.from_dict(item) for item in data.get("special_items", [])],
+            player_profiles = [SwPlayerProfile.from_dict(item) for item in data.get("player_profiles", [])],
+            player_effects = [SwPlayerEffect.from_dict(item) for item in data.get("player_effects", [])],
+            player_bans = [SwPlayerBan.from_dict(item) for item in data.get("player_bans", [])],
+            used_words = [item for item in data.get("used_words", [])],
+        )
+
+
+
+#region SwPlayerProfile   
+class SwPlayerProfile:
+    def __init__(self, user_id: int, user_name: str, user_display_name: str, point: int = 0, special_items: Optional[List['SwSpecialItem']] = None):
+        self.user_id = user_id 
+        self.user_name = user_name
+        self.user_display_name = user_display_name
+        self.point = point
+        self.special_items: List[SwSpecialItem] = special_items if special_items else []
+        
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "user_name": self.user_name,
+            "user_display_name": self.user_display_name,
+            "point": self.point,
+            "special_items": [data.to_dict() for data in self.special_items],
+        }
+
+    @staticmethod
+    def from_dict(data:dict):
+        return SwPlayerProfile(
+            user_id=data.get("user_id", None),
+            user_name=data.get("user_name", None),
+            user_display_name=data.get("user_display_name", None),
+            point=data.get("point", 0),
+            special_items = [SwSpecialItem.from_dict(item) for item in data.get("special_items", [])],
+        )
+
+
+#region SpecialItem
+class SwSpecialItem:
+    def __init__(self, item_id: str, item_name: str, item_description: str, level: str, quantity: int, point:int, required_target: bool, item_command):
+        self.item_id = item_id 
+        self.item_name = item_name
+        self.item_description = item_description
+        self.required_target = required_target
+        self.quantity = quantity
+        self.point = point
+        self.level = level
+        self.item_command =item_command
+    def to_dict(self):
+        return {
+            "item_id": self.item_id,
+            "item_name": self.item_name,
+            "item_description": self.item_description,
+            "quantity": self.quantity,
+            "point": self.point,
+            "level": self.level,
+            "required_target": self.required_target,
+            "item_command": self.item_command
+        }
+
+    @staticmethod
+    def from_dict(data:dict):
+        return SwSpecialItem(
+            item_id=data.get("item_id", None),
+            item_name=data.get("item_name", None),
+            item_description = data.get("item_description", None),
+            quantity = data.get("quantity", None),
+            point = data.get("point", None),
+            level = data.get("level", None),
+            required_target = data.get("required_target", None),
+            item_command = data.get("item_command", None),
+        )
+
+
+#region Player Effect
+class SwPlayerEffect:
+    def __init__(self, user_id: int, username: str, effect_id: str, effect_name: str, effect_command: str):
+        self.user_id = user_id 
+        self.username = username
+        self.effect_id = effect_id
+        self.effect_name = effect_name
+        self.effect_command = effect_command
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "username": self.username,
+            "effect_id": self.effect_id,
+            "effect_name": self.effect_name,
+            "effect_command": self.effect_command,
+        }
+        
+    @staticmethod
+    def from_dict(data:dict):
+        return SwPlayerEffect(
+            user_id=data.get("user_id", None),
+            username=data.get("username", None),
+            effect_id = data.get("effect_id", None),
+            effect_name = data.get("effect_name", None),
+        )
+
+
+#region Player Ban
+class SwPlayerBan:
+    def __init__(self, user_id: int, username: str, ban_remaining: int):
+        self.user_id = user_id 
+        self.username = username
+        self.ban_remaining = ban_remaining
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "username": self.username,
+            "ban_remaining": self.ban_remaining,
+        }
+        
+    @staticmethod
+    def from_dict(data:dict):
+        return SwPlayerBan(
+            user_id=data.get("user_id", None),
+            username=data.get("username", None),
+            ban_remaining = data.get("ban_remaining", 0),
+        )
---
+++ mini_game/SortWord/SwMongoManager.py
@@ -0,0 +1,256 @@
+import os
+from typing import List
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+from mini_game.SortWord.SwClass import SortWordInfo, SwPlayerProfile, SwSpecialItem, SwPlayerBan, SwPlayerEffect
+import random
+
+# Connect to the MongoDB server
+client = MongoClient("mongodb://localhost:27017/")
+# Create or switch to the database
+db_specific = client["sw_database"]
+
+#region SortWordInfo
+def find_sort_word_info_by_id(lang: str, guild_id: int, channel_id: int):
+    collection = db_specific[f'{lang}_sw_guild_{guild_id}']
+    data = collection.find_one({"channel_id": channel_id})
+    if data:
+        return SortWordInfo.from_dict(data)
+    return None
+
+def create_info(lang: str, guild_id: int, data: SortWordInfo):
+    #Mỗi channel là một collection riêng, chia theo channel id
+    collection = db_specific[f'{lang}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": data.channel_id})
+    if existing_data:
+        return f"Sort Word Info for this guild {guild_id}, channel {data.channel_id}, language {lang} already exists."
+    result = collection.insert_one(data.to_dict())
+    return result
+
+def update_data_info(channel_id: int, guild_id: int, current_player_id: int, current_player_name: str, current_word: str, lang: str, existed_words: List[str] = None, special_case: bool = False):
+    collection = db_specific[f'{lang}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    if existing_data == None:
+        #Không có thì tạo mới một SortWordInfo rỗng
+        new_data = SortWordInfo(channel_id = channel_id, channel_name="Uknown")
+        create_info(data=new_data, guild_id=guild_id)
+        existing_info = new_data
+    
+    used_words = existing_info.used_words
+    if existed_words != None:
+        used_words = existed_words
+    used_words.append(current_word)
+    
+    #Đảo lại current_word
+    char_list = list(current_word)
+    unsorted_word = ''.join(char_list)
+    while unsorted_word == current_word:
+        random.shuffle(char_list)
+        unsorted_word = ''.join(char_list)
+    result = collection.update_one({"channel_id": channel_id}, {"$set": {"current_player_id": current_player_id,
+                                                                         "current_player_name": current_player_name,
+                                                                         "current_word": current_word,
+                                                                         "unsorted_word": unsorted_word,
+                                                                         "special_case": special_case,
+                                                                         "used_words": [word for word in used_words], #chỉ dùng used_words
+                                                                         }})
+    return result
+
+def delete_data_info(channel_id: int, guild_id: int, language: str):
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    if existing_info:
+        #Xoá đi
+        result = collection.delete_one({"channel_id": channel_id})
+        return result
+    
+def update_special_point_data_info(channel_id: int, guild_id: int, language: str, special_point: int = 0):
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    #Cập nhật special point của channel lại
+    result = collection.update_one({"channel_id": channel_id}, {"$set": {"special_point": special_point,
+                                                                         }})
+    return result
+
+def update_special_item_word_matching_info(channel_id: int, guild_id: int, language: str, special_item: SwSpecialItem = None):
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    #Cập nhật special item của channel lại
+    if special_item:
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"special_item": special_item.to_dict(),
+                                                                         }})
+    else:
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"special_item": None,
+                                                                         }})
+    return result
+
+#region PlayerProfile
+def update_player_point_data_info(channel_id: int, guild_id: int, language: str,user_id: int, user_name: str, user_display_name: str, point:int):
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    list_player_profiles = existing_info.player_profiles
+    #Tìm xem có user_id trong list player_profiles chưa
+    selected_player = None
+    for player in list_player_profiles:
+        if player.user_id == user_id:
+            selected_player = player
+            break
+    if selected_player == None:
+        #Tạo mới player và thêm vào
+        new_player = SwPlayerProfile(user_id=user_id, username=user_name, user_display_name=user_display_name, point=point)
+        list_player_profiles.append(new_player)
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"player_profiles": [player.to_dict() for player in list_player_profiles],
+                                                                         }})
+        return result
+    else:
+        selected_player.point += point
+        if selected_player.point < 0: selected_player.point = 0
+        result = collection.update_one({"channel_id": channel_id, "player_profiles.user_id": user_id}, {"$set": {"player_profiles.$.point": selected_player.point,
+                                                                                                                }})
+        return result
+
+
+def update_player_special_item_word_matching_info(channel_id: int, guild_id: int, language: str,user_id: int,user_name: str, user_display_name: str, point:int, special_item: SwSpecialItem, remove_special_item = False):
+    """
+    Cập nhật lại danh sách các Special Items của player cụ thể.
+    """
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    list_player_profiles = existing_info.player_profiles
+    #Tìm xem có user_id trong list player_profiles chưa
+    selected_player = None
+    for player in list_player_profiles:
+        if player.user_id == user_id:
+            selected_player = player
+            break
+    if selected_player == None:
+        #Tạo mới player và thêm vào
+        temp = []
+        temp.append(special_item)
+        new_player = SwPlayerProfile(user_id=user_id, username=user_name, user_display_name=user_display_name, point=point, special_items=temp)
+        list_player_profiles.append(new_player)
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"player_profiles": [player.to_dict() for player in list_player_profiles],
+                                                                         }})
+        return result
+    else:
+        #Thêm item đó vào list special_items của player
+        existing_items = selected_player.special_items
+        if remove_special_item == False:
+            existing_items.append(special_item)
+        else:
+            #Tìm xem có item đó không, và remove luôn
+            for item_in_list in existing_items:
+                if item_in_list.item_id == special_item.item_id: 
+                    existing_items.remove(item_in_list)
+                    break
+        #Chỉ cho phép tối đa 3 item
+        if len(existing_items) > 3:
+            existing_items.pop(0)
+        result = collection.update_one({"channel_id": channel_id, "player_profiles.user_id": user_id}, {"$set": {"player_profiles.$.special_items": [data.to_dict() for data in existing_items],
+                                                                                                                }})
+        return result
+    
+#region Các functions về kỹ năng
+def update_current_player_id(channel_id: int, guild_id: int, language: str,user_id: int):
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    if existing_data:
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"current_player_id": user_id}})
+        return result
+    
+def update_all_players_point(channel_id: int, guild_id: int, language: str, immune_user_id: int,point: int):
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    if existing_info:
+        list_player_profiles = existing_info.player_profiles
+        for player_profile in list_player_profiles:
+            if player_profile.user_id == immune_user_id: 
+                continue
+            player_profile.point += point
+            if player_profile.point < 0: player_profile.point = 0
+        
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"player_profiles": [player.to_dict() for player in list_player_profiles],
+                                                                         }})
+        return result
+
+def update_player_effects(channel_id: int, guild_id: int, language: str,user_id: int,user_name: str, effect_id: str, effect_name: str, remove_special_effect = False):
+    """
+    Cập nhật lại danh sách Player Effects của player cụ thể.
+    """
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    list_player_effect = existing_info.player_effects
+    if remove_special_effect == False:
+        #Tạo mới danh sách Player Effect
+        data = SwPlayerEffect(user_id= user_id, username= user_name, effect_id= effect_id, effect_name= effect_name)
+        list_player_effect.append(data)
+    else:
+        #Xoá effect id của user id khỏi danh sách
+        for item in list_player_effect:
+            if item.effect_id == effect_id and item.user_id == user_id:
+                list_player_effect.remove(item)
+                break
+    list_temp_to_remove = []
+    for player_effect in list_player_effect:
+        if player_effect.user_id == user_id:
+            list_temp_to_remove.append(player_effect)
+            
+    if list_temp_to_remove != None and len(list_temp_to_remove)>2:
+        while len(list_temp_to_remove)>2:
+            first_skill_to_remove = list_temp_to_remove[0]
+            for item in list_player_effect:
+                if item.effect_id == first_skill_to_remove.effect_id and item.user_id == first_skill_to_remove.user_id:
+                    list_player_effect.remove(item)
+                    break
+            list_temp_to_remove.pop(0)
+        
+    #Cập nhật danh sách trong db
+    result = collection.update_one({"channel_id": channel_id}, {"$set": {"player_effects": [player_effects.to_dict() for player_effects in list_player_effect],
+                                                                         }})
+    return result
+
+def create_and_update_player_bans(channel_id: int, guild_id: int, language: str,user_id: int,user_name: str, ban_remaining: int):
+    """
+    Cập nhật lại danh sách Player Bans của world matching info cụ thể.
+    """
+    collection = db_specific[f'{language}_sw_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    list_player_ban = existing_info.player_bans
+    #Tìm xem có user_id trong list player_ban chưa
+    selected_player: SwPlayerBan = None
+    for player in list_player_ban:
+        if player.user_id == user_id:
+            selected_player = player
+            player.ban_remaining = ban_remaining
+            break
+    if selected_player == None:
+        #Tạo mới player ban và thêm vào world matching
+        new_player = SwPlayerBan(user_id=user_id, username=user_name, ban_remaining=ban_remaining)
+        list_player_ban.append(new_player)
+    elif selected_player.ban_remaining <= 0:
+        list_player_ban.remove(selected_player)
+    result = collection.update_one({"channel_id": channel_id}, {"$set": {"player_bans": [player.to_dict() for player in list_player_ban],
+                                                                         }})
+    return result
+    
+def reduce_player_bans_word_matching_info_after_round(channel_id: int, guild_id: int, language: str):
+    """
+    Trừ 1 điểm ban_remaining của tất cả dữ liệu trong danh sách Player Ban cụ thể sau mỗi lượt chơi thành công.
+    """
+    db_specific = client['word_matching_database']
+    collection = db_specific[f'{language}_word_matching_guild_{guild_id}']
+    existing_data = collection.find_one({"channel_id": channel_id})
+    existing_info = SortWordInfo.from_dict(existing_data)
+    list_player_ban = existing_info.player_bans
+    if list_player_ban != None and len(list_player_ban) > 0:
+        for player in list_player_ban:
+            player.ban_remaining += -1
+        new_list_remove_0 = [item for item in list_player_ban if item.ban_remaining > 0]    
+        result = collection.update_one({"channel_id": channel_id}, {"$set": {"player_bans": [player.to_dict() for player in new_list_remove_0],
+                                                                            }})
+        return result
