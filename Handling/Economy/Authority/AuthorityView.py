13ccdbb33e6ed3aafc9695e83f0086f6e63b9190
Nguyễn Văn Thanh Đức <ducducthanh2305@gmail.com>
2025-07-10 15:10:50 +0700

    update: reduce authority vote timeout, and allow server owner instantly conclude vote

--- Handling/Economy/Authority/AuthorityView.py
+++ Handling/Economy/Authority/AuthorityView.py
@@ -5,7 +5,7 @@ from Handling.Economy.Profile.ProfileClass import Profile
 
 class AuthorityView(discord.ui.View):
     def __init__(self, user: discord.Member, data: Profile):
-        super().__init__(timeout=60)
+        super().__init__(timeout=30)
         self.message: discord.Message = None
         self.embed: discord.Embed = None
         self.target_user = user
@@ -26,8 +26,15 @@ class AuthorityView(discord.ui.View):
         self.yes_votes.add(user.id)
 
         await interaction.response.send_message(f"Bạn đã đồng ý bầu cho {self.target_user.mention}!", ephemeral=True)
+        # Nếu là chủ server và tự vote cho mình => kết luôn
+        if user.id == self.target_user.id and user.id == interaction.guild.owner_id:
+            self.vote_concluded = True
+            self.yes_votes.add(1257713292445618239)
+            await self.conclude_vote(interaction)
+            return
+        
         # Kiểm tra xem đủ 10 vote chưa
-        if len(self.yes_votes) >= 10:
+        if len(self.yes_votes) >= 5:
             self.vote_concluded = True
             await self.conclude_vote(interaction)
 
@@ -40,13 +47,16 @@ class AuthorityView(discord.ui.View):
         self.no_votes.add(user.id)
 
         await interaction.response.send_message(f"Bạn đã không đồng ý bầu cho {self.target_user.mention}!", ephemeral=True)
-        # Kiểm tra xem đủ 10 vote chưa
-        if len(self.no_votes) >= 10:
+        # Kiểm tra xem đủ 5 vote chưa
+        if len(self.no_votes) >= 5:
             self.vote_concluded = True
             await self.conclude_vote(interaction)
 
     async def conclude_vote(self, interaction: discord.Interaction=None):
-        await self.message.edit(embed=self.embed, view= None)
+        try:
+            await self.message.edit(embed=self.embed, view= None)
+        except Exception as e:
+            pass
         if len(self.yes_votes) > len(self.no_votes):
             result_message = f"**{self.target_user.display_name}** đã thắng bầu cử và trở thành Chính Quyền! Có **{len(self.yes_votes)}** người đã bầu ủng hộ! Đã cộng thêm tiền và của cải cho tân Chính Quyền đương nhiệm!"
             ProfileMongoManager.set_authority(guild_id=self.target_user.guild.id, guild_name=self.target_user.guild.name, user_id=self.target_user.id,user_name=self.target_user.name,user_display_name= self.target_user.display_name)
@@ -56,7 +66,6 @@ class AuthorityView(discord.ui.View):
         else:
             result_message = f"**{self.target_user.display_name}** đã thua bầu cử! Đáng tiếc là chỉ có {len(self.yes_votes)} người bầu ủng hộ bạn! Đừng quên bạn cũng vừa bị trừ **500** <a:copper:1294615524918956052>!"
             ProfileMongoManager.update_profile_money(guild_id=self.target_user.guild.id, guild_name=self.target_user.guild.name, user_id=self.target_user.id,user_name=self.target_user.name,user_display_name= self.target_user.display_name,copper=-500)
-        
         embed = discord.Embed(title=f"Chính Quyền Đương Cử",description=f"{result_message}",color=discord.Color.blue())
         embed.add_field(name=f"", value="▬▬▬▬▬ι═══════════>", inline=False)
         embed.add_field(name=f"", value="\n", inline=False)
