a2b4dfc4f21e66191e20218adf96ea5003499cd1
satthukid000 <satthubongmoi2305@gmail.com>
2026-01-05 20:44:53 +0700

    update: add id and is upper side check

--- Handling/Economy/GA/GuardianAngelAttackClass.py
+++ Handling/Economy/GA/GuardianAngelAttackClass.py
@@ -4,9 +4,12 @@ from Handling.Economy.Profile.ProfileClass import Profile
 from Handling.Economy.GA.GuardianAngelClass import GuardianAngel
 import Handling.Economy.GA.ListGAAndSkills as ListGAAndSkills
 from CustomEnum.EmojiEnum import EmojiCreation2
+import uuid
+
 
 class GuardianAngelAttackClass:
-    def __init__(self, player_ga: GuardianAngel, player_profile: Profile = None, starting_at_round:int = 1, recovery_time:int = 0, max_recovery:int = 2, max_mass_restored_mana_skill: int = 2, is_used_skill_critical_strike = False, brain_washed_round: int = None, stunned_round: int = 0, is_dead_ga = False, has_used_summoning = False, is_summoned = False, summoner_owner_id: int = None, is_used_skill_resurrection = False, max_shield = 3, max_summon_sacrifice = 1):
+    def __init__(self, player_ga: GuardianAngel, player_profile: Profile = None, starting_at_round:int = 1, recovery_time:int = 0, max_recovery:int = 2, max_mass_restored_mana_skill: int = 2, is_used_skill_critical_strike = False, brain_washed_round: int = 0, stunned_round: int = 0, is_dead_ga = False, has_used_summoning = False, is_summoned = False, summoner_owner_id: int = None, is_used_skill_resurrection = False, max_shield = 3, max_summon_sacrifice = 1, is_upper_side = True):
+        self.ga_attack_uid = str(uuid.uuid4())
         self.player_profile = player_profile
         self.player_ga = player_ga
         self.starting_at_round = starting_at_round
@@ -27,3 +30,5 @@ class GuardianAngelAttackClass:
         
         self.max_shield = max_shield
         self.max_summon_sacrifice = max_summon_sacrifice
+        
+        self.is_upper_side = is_upper_side
